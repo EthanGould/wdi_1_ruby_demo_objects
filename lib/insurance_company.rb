9e436743c7651817a9ffbbb1fc67ebdffa59b4bb
Ethan Gould <ethan.g.907@gmail.com>
2014-04-24 14:29:41 -0400

    add featue to add clients and made array to save new clients into

--- lib/insurance_company.rb
+++ lib/insurance_company.rb
@@ -1,15 +1,22 @@
 # InsuranceCompany class
 
 class InsuranceCompany
+  attr_reader :name
 
   def initialize(company_name)
     #instance variable, MUST start with '@'
     @name = company_name
+    @client = []
   end
 
-  # getter
-  def name
-    @name
+  #add person as a client
+  def add_client(person)
+    @client << person
+  end
+
+  #get all my clients
+  def get_clients
+    clients
   end
 
 end
